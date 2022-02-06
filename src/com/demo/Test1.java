eac9fc618e1eee5db397bf5dc2769721901bb7e0
rajiv tripathi <rajivtripathi.july10@gmail.com>
2022-02-06 20:38:04 +0530

    done

---
+++ src/com/demo/Test1.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" this is 1st line");
+		System.out.println(" this is 2nd line");
+		
+
+	}
+
+}
