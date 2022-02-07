c57e04c18c55e0a6b8224b9ab9aad580f5054644
Rajiv Tripathi <rajivtripathi.july10@gmail.com>
2022-02-08 00:29:07 +0530

    Updated text remote

--- src/com/demo/Test1.java
+++ src/com/demo/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" this is 1st line I have changes it");
+		System.out.println(" this is 1st line I have changes it. Updated to check remote");
 		System.out.println(" this is 2nd line this is changes");
 		
 
