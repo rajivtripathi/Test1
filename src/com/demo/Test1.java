12ef7b8ec9c7ca42f664b7339a752fdd2b6c50f6
Rajiv Tripathi <rajivtripathi.july10@gmail.com>
2022-02-08 10:36:26 +0530

    added in test1 remote

--- src/com/demo/Test1.java
+++ src/com/demo/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 
+		sysout("added in test1 remote");
 		System.out.println(" this is 1st line I have changes it. Updated to check remote");
 		System.out.println(" this is 2nd line this is changes");
 
